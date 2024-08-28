99d26d26accf69527cb7bddccc6bb542bb658e8f
kumar-music-vcpb <79351879+kumar-music-vcpb@users.noreply.github.com>
2024-08-28 18:34:57 +0530

    Update ai_LazyDeveloper.py

--- ai_LazyDeveloper.py
+++ ai_LazyDeveloper.py
@@ -1,6 +1,6 @@
 #
 #         i am NOT INTRESTED IN BEING YOUR SECOND FATHER... SO DON'T REMOVE MY CREDIT...
-#                                       @LazyDeveloperr
+#                                       @Kumar_Manis
 # 
 # 
 # 
@@ -15,7 +15,7 @@
 #                                                ...PRESENTING...
 #                                  A R T I F i C I A L  -  I N T E L i G E N C E
 #                                              .in  Auto-Filter-Bot.
-#                                                 @LazyDeveloperr
+#                                                 @Kumar_Manis
 #
 #                                       
 #
@@ -34,7 +34,7 @@
 #
 # Credit @LazyDeveloper.
 # Please Don't remove credit.
-# Born to make history @LazyDeveloper !
-# Thank you LazyDeveloper for helping us in this Journey
-# 🥰  Thank you for giving me credit @LazyDeveloperr  🥰
-# for any error please contact me -> telegram@LazyDeveloperr or insta @LazyDeveloperr 
+# Born to make history @Kumar_Manis !
+# Thank you @Nacked_Eye 🔑 for helping us in this Journey
+# 🥰  Thank you for giving me credit @Kumar_Manis  🥰
+# for any error please contact me -> telegram@Kumar_Manis or insta @Kumar_Manis 
